60f39d695a771ea50c40ebffaf115fe26bdadc10
AhmedGhazala2000 <ahmed.ghazala142000@gmail.com>
2024-05-24 21:33:24 +0300

    Add blocObservers

---
+++ lib/Core/utils/simple_bloc_observers.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SimpleBlocObservers extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    debugPrint('onChange: $change');
+  }
+
+  // @override
+  // void onClose(BlocBase bloc) {
+  //   super.onClose(bloc);
+  //   debugPrint('onClose: $bloc');
+  // }
+  //
+  // @override
+  // void onCreate(BlocBase bloc) {
+  //   super.onCreate(bloc);
+  //   debugPrint('onCreate: $bloc');
+  // }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'Core/utils/simple_bloc_observers.dart';
 import 'Features/Splash/presentation/views/splash_view.dart';
 
 void main() {
+  Bloc.observer = SimpleBlocObservers();
+
   runApp(const ToDoApp());
 }
 
