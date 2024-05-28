7ef1268ab648f0640c805d86c2d8df41a71235d2
AhmedGhazala2000 <ahmed.ghazala142000@gmail.com>
2024-05-28 03:22:31 +0300

    Finish home view UI

--- lib/Features/Home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:todo_app/Core/utils/styles.dart';
 import 'custom_home_app_bar.dart';
 import 'select_tasks_items_list_view.dart';
